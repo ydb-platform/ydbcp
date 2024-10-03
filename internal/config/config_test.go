8dde5971a5bfbff87175652dc95a0bd099fad2aa
Mikhail Babich <mbabich@nebius.com>
2024-10-03 17:21:39 +0200

    set default values for configuration parameters

---
+++ internal/config/config_test.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigDefaults(t *testing.T) {
+	ctx := context.Background()
+	f, err := os.CreateTemp("", "config")
+	assert.Empty(t, err)
+	fileName := f.Name()
+	defer os.Remove(fileName)
+	assert.Empty(t, f.Close())
+	cfg, err := InitConfig(ctx, fileName)
+	assert.Empty(t, err)
+	assert.Equal(t, uint16(8080), cfg.MetricsServer.BindPort)
+}
