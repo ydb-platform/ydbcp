7744d4eb3c9dffd67df393be5edcc105c98fe114
Iuliia Sidorina <yulia@ydb.tech>
2025-01-21 12:13:05 +0100

    Revert "refactor(operations): use full names for operation types (#128)" (#131)
    This reverts commit 90afa3a9884965b4aaac3ef2a787757fd73ca69f.

--- internal/types/operation.go
+++ internal/types/operation.go
@@ -434,20 +434,11 @@ var (
 	OperationStateStartCancelling = OperationState(pb.Operation_START_CANCELLING.String())
 )
 
-// Deprecated: these short operation type names are unclear and will be removed in the future. Use full names instead.
 const (
-	LegacyOperationTypeTB   = OperationType("TB")
-	LegacyOperationTypeRB   = OperationType("RB")
-	LegacyOperationTypeDB   = OperationType("DB")
-	LegacyOperationTypeTBWR = OperationType("TBWR")
-)
-
-const (
-	OperationTypeTB   = OperationType("TakeBackup")
-	OperationTypeRB   = OperationType("RestoreBackup")
-	OperationTypeDB   = OperationType("DeleteBackup")
-	OperationTypeTBWR = OperationType("TakeBackupWithRetries")
-
+	OperationTypeTB       = OperationType("TB")
+	OperationTypeRB       = OperationType("RB")
+	OperationTypeDB       = OperationType("DB")
+	OperationTypeTBWR     = OperationType("TBWR")
 	BackupTimestampFormat = "20060102_150405"
 	OperationCreatorName  = "ydbcp"
 )
